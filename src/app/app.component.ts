37ae2e1485c9a59605fd8f5aaebe78b75d0fff5c
doomys1234 <doomys1234@gmail.com>
2022-07-02 23:02:01 +0300

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
+
+import { Observable, fromEvent, merge, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-timer';
+export class AppComponent implements AfterViewInit {
+  title = 'angular-test-project';
+
+  interval: any;
+  timerPaused: Boolean = false;
+  running: Boolean = false;
+  counter: number = 0;
+  seconds: number = 0;
+  minutes: number = 0;
+
+  stream$ = new Observable((observer) => {
+    this.interval = setInterval(() => {
+      observer.next(this.counter++);
+    }, 1000);
+  });
+
+  @ViewChild('secondsText')
+  secondsVal!: ElementRef;
+  @ViewChild('minutesText')
+  minutesVal!: ElementRef;
+  @ViewChild('pauseButton')
+  pauseButton!: ElementRef;
+
+  onStartClick(event: any) {
+    if (!this.running) {
+      this.running = true;
+      this.timerPaused =false
+      this.stream$.subscribe((val) => {
+        this.seconds++;
+        this.secondsVal.nativeElement.textContent = `0${this.seconds} seconds`;
+
+        if (this.seconds > 9) {
+          this.secondsVal.nativeElement.textContent = `${this.seconds} seconds`;
+        }
+        if (this.seconds === 60) {
+          this.minutes++;
+          this.seconds = 0;
+          this.minutesVal.nativeElement.textContent = `0${this.minutes} minutes :`;
+          this.secondsVal.nativeElement.textContent = `0${this.seconds} seconds`;
+        }
+
+        if (this.minutes > 9) {
+          this.minutesVal.nativeElement.textContent = `${this.minutes} minutes :`;
+        }
+      });
+    }
+  }
+
+  onResetClick(event: any) {
+    if (this.running || this.timerPaused) {
+      clearInterval(this.interval);
+      this.seconds = 0;
+      this.minutes = 0;
+      this.secondsVal.nativeElement.textContent = `0${this.seconds} seconds`;
+      this.minutesVal.nativeElement.textContent = `0${this.minutes} minutes :`;
+      this.running = false;
+      this.timerPaused = false;
+    }
+  }
+
+  ngAfterViewInit(): void {
+    const pauseButton = this.pauseButton.nativeElement;
+    const clickEvent = fromEvent<MouseEvent>(pauseButton, 'click');
+    const dblClickEvent = fromEvent<MouseEvent>(pauseButton, 'dblclick');
+    const eventsMerged = merge(clickEvent, dblClickEvent).pipe(
+      debounceTime(500)
+    );
+    eventsMerged.subscribe((event) => {
+      if (event.type === 'dblclick') {
+        pauseButton.textContent = 'Pause';
+        
+        clearInterval(this.interval);
+        this.timerPaused = true;
+        this.running = false;
+        return;
+      }
+      pauseButton.textContent = 'Press twice';
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule, } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppComponent } from './app.component';
 
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+ 
+}
